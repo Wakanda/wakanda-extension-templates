7e89d99e4dc768caf9587ad06392978588837c0b
Yassine Boukhata <yacine_r@yahoo.fr>
2016-09-20 12:08:29 +0200

    update angular2 webpack template

--- web/angular2-webpack/src/vendor.ts
+++ web/angular2-webpack/src/vendor.ts
@@ -6,11 +6,11 @@ import '@angular/common';
 import '@angular/http';
 import '@angular/router';
 
-// RxJS
 import 'rxjs';
+import '@angularclass/hmr';
 
 // import wakanda client
-import 'wakanda-client';
+import 'wakanda-client/browser/no-promise';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
