3f0bd3fda38ff29a481296d69d9f1dd7ce9aedc8
Yassine Boukhata <yacine_r@yahoo.fr>
2016-11-28 13:19:15 +0100

    update ionic2 blank template

--- mobile/ionic2-blank-template/src/pages/home/home.ts
+++ mobile/ionic2-blank-template/src/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+
 import { NavController } from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController) {
+    
   }
+
 }
